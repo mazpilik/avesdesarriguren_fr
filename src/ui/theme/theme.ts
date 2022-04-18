c8e1588ddb63ede016bcb87acd233c1839df3a66
mazpilic <mazpilic@gmail.com>
2022-04-18 12:01:59 +0200

    add theme colors for admin text,, titles and buttons

--- src/ui/theme/theme.ts
+++ src/ui/theme/theme.ts
@@ -6,6 +6,20 @@ export const lightTheme: DefaultTheme = {
     mainMenuText: '#FFFFFF',
     bodyBackground: '#F0EFEB',
     userCardBackground: '#ebebeb',
+    textColor: '#666666',
+    titleColor: '#000000',
+    AddBtnColor: {
+      default: '#BEE1E6',
+      hover: '#BEE1E6',
+    },
+    EditBtnColor: {
+      default: '#CDDAFD',
+      hover: '#CDDAFD',
+    },
+    DeleteBtnColor: {
+      default: '#FDE2E4',
+      hover: '#FDE2E4',
+    },
   },
 };
 export const darkTheme: DefaultTheme = {};
