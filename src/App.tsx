e6a0e342e3151671c9a359ebc263ea251bdc1d20
mazpilic <mazpilic@gmail.com>
2022-04-18 22:59:46 +0200

    add new sections privateRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,11 @@ import { PrivateRoute } from './ui/_components/PrivateRoute/PrivateRoute';
 import { UserChecker } from './ui/_components/UserChecker/UserChecker';
 import { ThemeProviderWrapper } from './ui/theme/ThemeProviderWrapper';
 import { LangProvider } from './ui/_components/LangProvider/LangProvider';
+import { AddBird } from './ui/pages/Admin/Bird/AddBird';
+import { AddFamily } from './ui/pages/Admin/Family/AddFamily';
+import { AddOrder } from './ui/pages/Admin/Order/AddOrder';
+import { AddNews } from './ui/pages/Admin/News/AddNews';
+import { AddUser } from './ui/pages/Admin/User/AddUser';
 
 const App: FC = () => (
   <BrowserRouter>
@@ -28,6 +33,11 @@ const App: FC = () => (
             <Route path={routes.ACCESS_POINT} element={<Home />} />
             <Route path={routes.LOGIN} element={<Login />} />
             <Route path={routes.ADMIN_DASHBOARD} element={<PrivateRoute component={Dashboard} />} />
+            <Route path={routes.ADD_BIRD} element={<PrivateRoute component={AddBird} />} />
+            <Route path={routes.ADD_FAMILY} element={<PrivateRoute component={AddFamily} />} />
+            <Route path={routes.ADD_ORDER} element={<PrivateRoute component={AddOrder} />} />
+            <Route path={routes.ADD_NEWS} element={<PrivateRoute component={AddNews} />} />
+            <Route path={routes.ADD_USER} element={<PrivateRoute component={AddUser} />} />
           </Routes>
         </LangProvider>
       </ThemeProviderWrapper>
