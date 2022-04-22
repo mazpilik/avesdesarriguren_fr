ddc8fe789da9aa5711050533449d9baacaaf2360
mazpilic <mazpilic@gmail.com>
2022-04-23 00:40:32 +0200

    add new elements to theme

--- src/ui/theme/theme.ts
+++ src/ui/theme/theme.ts
@@ -2,24 +2,26 @@ import { DefaultTheme } from 'styled-components';
 
 export const lightTheme: DefaultTheme = {
   admin: {
-    mainMenuBack: '#14213D',
-    mainMenuText: '#FFFFFF',
-    bodyBackground: '#F0EFEB',
-    userCardBackground: '#ebebeb',
-    textColor: '#666666',
-    titleColor: '#000000',
     AddBtnColor: {
       default: '#BEE1E6',
       hover: '#BEE1E6',
+    },
+    bodyBackground: '#F0EFEB',
+    DeleteBtnColor: {
+      default: '#FDE2E4',
+      hover: '#FDE2E4',
     },
     EditBtnColor: {
       default: '#CDDAFD',
       hover: '#CDDAFD',
     },
-    DeleteBtnColor: {
-      default: '#FDE2E4',
-      hover: '#FDE2E4',
-    },
+    ElementsBorder: '#ccc',
+    ElementsBackground: '#FFFFFF',
+    mainMenuBack: '#14213D',
+    mainMenuText: '#FFFFFF',
+    textColor: '#666666',
+    titleColor: '#000000',
+    userCardBackground: '#ebebeb',
   },
 };
 export const darkTheme: DefaultTheme = {};
