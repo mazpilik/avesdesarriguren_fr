adb30c10a1e19ab1f410a930de77ffe154070942
mazpilic <mazpilic@gmail.com>
2022-04-23 00:40:46 +0200

    add components to app

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,19 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
-import { Dashboard } from './ui/pages/Admin/Dashboard';
-import { Home } from './ui/pages/Home';
-import { Login } from './ui/pages/Login';
-import { PrivateRoute } from './ui/_components/PrivateRoute/PrivateRoute';
-import { UserChecker } from './ui/_components/UserChecker/UserChecker';
-import { ThemeProviderWrapper } from './ui/theme/ThemeProviderWrapper';
-import { LangProvider } from './ui/_components/LangProvider/LangProvider';
 import { AddBird } from './ui/pages/Admin/Bird/AddBird';
 import { AddFamily } from './ui/pages/Admin/Family/AddFamily';
-import { AddOrder } from './ui/pages/Admin/Order/AddOrder';
 import { AddNews } from './ui/pages/Admin/News/AddNews';
+import { AddOrder } from './ui/pages/Admin/Order/AddOrder';
 import { AddUser } from './ui/pages/Admin/User/AddUser';
+import { Dashboard } from './ui/pages/Admin/Dashboard';
+import { Home } from './ui/pages/Home';
+import { LangProvider } from './ui/_components/LangProvider/LangProvider';
+import { ListOrders } from './ui/pages/Admin/Order/ListOrders';
+import { Login } from './ui/pages/Login';
+import { PrivateRoute } from './ui/_components/PrivateRoute/PrivateRoute';
+import { ThemeProviderWrapper } from './ui/theme/ThemeProviderWrapper';
+import { UserChecker } from './ui/_components/UserChecker/UserChecker';
 
 const App: FC = () => (
   <BrowserRouter>
@@ -36,6 +37,7 @@ const App: FC = () => (
             <Route path={routes.ADD_BIRD} element={<PrivateRoute component={AddBird} />} />
             <Route path={routes.ADD_FAMILY} element={<PrivateRoute component={AddFamily} />} />
             <Route path={routes.ADD_ORDER} element={<PrivateRoute component={AddOrder} />} />
+            <Route path={routes.LIST_ORDER} element={<PrivateRoute component={ListOrders} />} />
             <Route path={routes.ADD_NEWS} element={<PrivateRoute component={AddNews} />} />
             <Route path={routes.ADD_USER} element={<PrivateRoute component={AddUser} />} />
           </Routes>
