2e9d9d2ab5df9f1f03c5e44e162a4177ef635731
mazpilic <mazpilic@gmail.com>
2022-05-03 22:45:56 +0200

    add forms global styles

--- src/ui/theme/global.ts
+++ src/ui/theme/global.ts
@@ -1,5 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-
+  .p-float-label{
+    .p-dropdown.p-component {
+      min-width: 250px;
+    }
+    .p-inputtext.p-component {
+      min-width: 250px;
+    }
+  }
+  .sh-field-wrapper{
+    margin-bottom: 1.5rem;
+  }
 `;
