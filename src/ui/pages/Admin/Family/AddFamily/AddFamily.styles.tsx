f25f200bc6f80a6cb974a37ecb9760a32dc5d798
mazpilic <mazpilic@gmail.com>
2022-05-03 22:45:03 +0200

    add AddFamily section

---
+++ src/ui/pages/Admin/Family/AddFamily/AddFamily.styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components/macro';
+import { Card } from 'primereact/card';
+
+export const ActionButtons = styled.div`
+  margin-top: 2.5rem;
+`;
+export const FieldWrapper = styled.div`
+  margin-bottom: 1rem;
+`;
+export const SectionCard = styled(Card)``;
