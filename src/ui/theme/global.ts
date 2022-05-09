278e59b315bd817c69033d0abbbff2a12aa51032
mazpilic <mazpilic@gmail.com>
2022-05-09 08:12:18 +0200

    global styles over primereact components and colors of buttons in theme

--- src/ui/theme/global.ts
+++ src/ui/theme/global.ts
@@ -3,13 +3,20 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   .p-float-label{
     .p-dropdown.p-component {
-      min-width: 250px;
+      min-width: 500px;
     }
     .p-inputtext.p-component {
-      min-width: 250px;
+      min-width: 500px;
+    }
+    .p-inputtextarea.p-component {
+      min-width: 500px;
+      min-height: 250px;
+    }
+    .p-multiselect.p-component {
+      min-width: 500px;
     }
   }
   .sh-field-wrapper{
-    margin-bottom: 1.5rem;
+    margin-bottom: 3rem;
   }
 `;
--- src/ui/theme/theme.ts
+++ src/ui/theme/theme.ts
@@ -5,19 +5,23 @@ export const lightTheme: DefaultTheme = {
     AddBtnColor: {
       default: '#BEE1E6',
       hover: '#BEE1E6',
+      disabled: '#BEE1E6',
     },
     bodyBackground: '#F0EFEB',
     DeleteBtnColor: {
       default: '#FDE2E4',
       hover: '#FDE2E4',
+      disabled: '#BEE1E6',
     },
     EditBtnColor: {
       default: '#CDDAFD',
       hover: '#CDDAFD',
+      disabled: '#BEE1E6',
     },
     CancelBtnColor: {
       default: '#ebebeb',
       hover: '#ebebeb',
+      disabled: '#BEE1E6',
     },
     ElementsBorder: '#ccc',
     ElementsBackground: '#FFFFFF',
