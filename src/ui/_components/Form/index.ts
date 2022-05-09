705dac8ac429312411aab5f34d36e3c8b848fbe4
mazpilic <mazpilic@gmail.com>
2022-05-09 08:07:08 +0200

    add form global styles and components

---
+++ src/ui/_components/Form/index.ts
@@ -0,0 +1,9 @@
+import { ActionButtons, FieldWrapper, FieldHelp } from './Form.styles';
+import { FloatLabelField } from './FormComponents';
+
+export {
+  ActionButtons,
+  FieldWrapper,
+  FloatLabelField,
+  FieldHelp,
+};
